ab5fc5a7764c5e283c9c49c6765f6b96473e4b3f
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-15 17:41:47 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase = "Squawk!")
-  puts phrase
-  return phrase
+def parrot(noise = "Squawk!")
+  puts "#{noise}"
+  "#{noise}"
 end
+
+parrot
